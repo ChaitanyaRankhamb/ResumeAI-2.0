ebd9de14279bb1eebea5fafae25afd9cacb9ff76
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-04-08 19:04:20 +0530

    added get analyzed data route

--- Backend/src/modules/resume/resume.routes.ts
+++ Backend/src/modules/resume/resume.routes.ts
@@ -1,11 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { authMiddleware } from "../../middlewares/auth.middleware";
 import { uploadResumeController } from "./controllers/resume.controller";
+import { getResumeAnalysisController } from "./controllers/getResumeAnalysis.controller";
 import { uploadResumeMiddleware } from "../../middlewares/upload.resume.middleware";
 
 const router = express.Router();
 
+// Define params type for progress route
+interface ProgressParams {
+  fileId: string;
+}
+
 // route to upload resume  it should be protected
-router.post("/", authMiddleware, uploadResumeMiddleware, uploadResumeController);
+router.post(
+  "/",
+  authMiddleware,
+  uploadResumeMiddleware,
+  uploadResumeController,
+);
+
+// Get analyzed resume report data (protected)
+router.get("/analysis/:fileId", authMiddleware, getResumeAnalysisController);
 
 export default router;
