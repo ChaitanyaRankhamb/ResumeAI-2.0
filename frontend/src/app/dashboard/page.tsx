53d972d4f9f272a7d71c1bae4d0657202f0653fc
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-07-27 12:10:00 +0530

    remoce useRouter

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
-import { DashboardNavbar } from "@/components/dashboard/navbar";
 import { UploadResumeSection } from "@/components/dashboard/upload-resume-section";
-import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
-  const router = useRouter();
-
   return (
     <>
       <UploadResumeSection />
